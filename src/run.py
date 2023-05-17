8b72d9ba11be171029f55b29b1713c6655b3a6cd
tischrei <tino.schreiber@t-systems.com>
2023-05-17 13:22:31 +0000

    modify run.py

--- src/run.py
+++ src/run.py
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import openstack
 
 cloud   = "otcdomain"       # your clouds.yaml cloud environment comes here
@@ -10,29 +22,33 @@ def userdetails(userid):
         return "formerly user #" + userid, "UNKNOWN OWNER"
     return user.name, user.email
 
-# Create a working connection to OpenStack
-conn = openstack.connect(cloud=cloud)
+def main():
+    # Create a working connection to OpenStack
+    conn = openstack.connect(cloud=cloud)
 
-projects = [(project.id,
-             project.name,
-             conn.connect_as_project(project=project.name))
-            for project in conn.identity.projects() if region in project.name]
+    projects = [(project.id,
+                project.name,
+                conn.connect_as_project(project=project.name))
+                for project in conn.identity.projects() if region in project.name]
 
-for pid, pname, pco in projects:
-    print(f"Project {pname} ({pid}):")
+    for pid, pname, pco in projects:
+        print(f"Project {pname} ({pid}):")
 
-    # Retrieve all servers in the project
-    servers = [(server.id,
-                server.name,
-                server.user_id,
-                server.created_at)
-               for server in pco.compute.servers()]
-    for id, name, ownerid, created in servers:
-        owner, email = userdetails(ownerid)
-        print(f"  Server {id} ({name}) {owner}")
-        print(f"         created {created} by {email}")
+        # Retrieve all servers in the project
+        servers = [(server.id,
+                    server.name,
+                    server.user_id,
+                    server.created_at)
+                for server in pco.compute.servers()]
+        for id, name, ownerid, created in servers:
+            owner, email = userdetails(ownerid)
+            print(f"  Server {id} ({name}) {owner}")
+            print(f"         created {created} by {email}")
 
-        attachments = [(volume.id, volume.name) for volume in pco.compute.volume_attachments(server=id)]
-        for id, name in attachments:
-            vtype = pco.volume.find_volume(id).volume_type
-            print(f"      Volume {id} ({name}): {vtype}")
+            attachments = [(volume.id, volume.name) for volume in pco.compute.volume_attachments(server=id)]
+            for id, name in attachments:
+                vtype = pco.volume.find_volume(id).volume_type
+                print(f"      Volume {id} ({name}): {vtype}")
+
+if __name__ == '__main__':
+    main()
